ecba58b9a72f8a2452d243ecc96f9ce3d9f1d098
manish20002 <manishthirani202@gmail.com>
2022-10-07 11:44:39 +0530

    Updates

---
+++ precache-manifest.55eb6d5c58fab9e37a94df5e322e1217.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "62cea5aace546504ecc5",
+    "url": "/Wallpapersz/static/css/main.36e413ae.chunk.css"
+  },
+  {
+    "revision": "62cea5aace546504ecc5",
+    "url": "/Wallpapersz/static/js/main.62cea5aa.chunk.js"
+  },
+  {
+    "revision": "546dafc18c5050b214b1",
+    "url": "/Wallpapersz/static/js/1.546dafc1.chunk.js"
+  },
+  {
+    "revision": "834e63f300cefd170981",
+    "url": "/Wallpapersz/static/js/2.834e63f3.chunk.js"
+  },
+  {
+    "revision": "74ac302c725b781c410e",
+    "url": "/Wallpapersz/static/js/runtime~main.74ac302c.js"
+  },
+  {
+    "revision": "f69194599bcd8ec0fd6c1eff36360185",
+    "url": "/Wallpapersz/static/media/Gaming.f6919459.png"
+  },
+  {
+    "revision": "803cb765e9963c582b2fa57e7986d06a",
+    "url": "/Wallpapersz/static/media/wolverine.803cb765.png"
+  },
+  {
+    "revision": "003e46f98027df77f9aab3d1e97a9a4a",
+    "url": "/Wallpapersz/static/media/boruto.003e46f9.png"
+  },
+  {
+    "revision": "88606a1cd9e5e974c585299f8ed9b576",
+    "url": "/Wallpapersz/static/media/moon.88606a1c.jpg"
+  },
+  {
+    "revision": "7c0df03ae909ed88729bdd47e01a4bb0",
+    "url": "/Wallpapersz/static/media/manish.7c0df03a.PNG"
+  },
+  {
+    "revision": "4e0a017acc4a8c941edbd6d5f1dca806",
+    "url": "/Wallpapersz/static/media/Ritesh.4e0a017a.jpg"
+  },
+  {
+    "revision": "e7d197ab977d99dc2c7aa0bfcff6f02f",
+    "url": "/Wallpapersz/static/media/logo1.e7d197ab.svg"
+  },
+  {
+    "revision": "94ec1d04094b329e35503320f86ac8a2",
+    "url": "/Wallpapersz/static/media/Bg1.94ec1d04.JPG"
+  },
+  {
+    "revision": "66bbd2781f9d8024a11b90b0aef1f918",
+    "url": "/Wallpapersz/static/media/errorpg.66bbd278.jpg"
+  },
+  {
+    "revision": "01a229af52030b90b78553043fa49266",
+    "url": "/Wallpapersz/index.html"
+  }
+];
